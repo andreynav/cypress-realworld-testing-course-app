ea49d491c32e3a81c738e6f5580be539f827f152
andreinalivaika <nav.testsw@gmail.com>
2023-05-25 11:53:24 +0300

    Install cypress and first tests

---
+++ cypress/e2e/home.cy.ts
@@ -0,0 +1,15 @@
+describe('Home test', () => {
+  beforeEach(() => {
+        cy.visit('http://localhost:3000/')
+  })
+
+  it('the h1 contains correct text', () => {
+    cy.get('[data-test="hero-heading"]').contains('Testing Next.js Applications with Cypress')
+  })
+
+  it('the features on the home page are correct', () => {
+    cy.get('dt').eq(0).contains('4 Courses')
+    cy.get('dt').eq(1).contains('25+ Lessons')
+    cy.get('dt').eq(2).contains('Free and Open Source')
+  })
+})
